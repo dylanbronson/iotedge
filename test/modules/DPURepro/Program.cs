ed5b48fec826f4a2d72d5d356374d6da83d2e241
dylanbronson <dybronso@microsoft.com>
2020-04-01 09:12:31 -0700

    Repro for DPU issu

---
+++ test/modules/DPURepro/Program.cs
@@ -0,0 +1,39 @@
+namespace DPURepro
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Edge.ModuleUtil;
+    using Microsoft.Azure.Devices.Edge.Util;
+    using Microsoft.Azure.Devices.Shared;
+    using Microsoft.Extensions.Logging;
+
+    class Program
+    {
+        static readonly ILogger Logger = ModuleUtil.CreateLogger(nameof(DPURepro));
+
+        static async Task Main(string[] args)
+        {
+            (CancellationTokenSource cts, ManualResetEventSlim completed, Option<object> handler) = ShutdownHandler.Init(TimeSpan.FromSeconds(5), Logger);
+
+            ModuleClient moduleClient = await ModuleUtil.CreateModuleClientAsync(
+                        TransportType.Amqp,
+                        ModuleUtil.DefaultTimeoutErrorDetectionStrategy,
+                        ModuleUtil.DefaultTransientRetryStrategy,
+                        Logger);
+            await moduleClient.SetDesiredPropertyUpdateCallbackAsync(ReceivedUpdate, null);
+
+            await cts.Token.WhenCanceled();
+            completed.Set();
+            handler.ForEach(h => GC.KeepAlive(h));
+            Logger.LogInformation("TwinTester exiting.");
+        }
+
+        static Task ReceivedUpdate(TwinCollection twinCollection, object _)
+        {
+            Logger.LogInformation($"Received DesiredPropertyUpdate callback: {twinCollection.ToString()}");
+            return Task.CompletedTask;
+        }
+    }
+}
