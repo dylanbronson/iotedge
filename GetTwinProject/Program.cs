9e03eeb289ec728b16aaed483ac20ea557692491
dylanbronson <dybronso@microsoft.com>
2020-07-24 10:15:16 -0700

    multiple twins

--- GetTwinProject/Program.cs
+++ GetTwinProject/Program.cs
@@ -12,7 +12,13 @@ namespace GetTwinProject
         {
             RegistryManager rm = RegistryManager.CreateFromConnectionString("HostName=dybronso-pnp-hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=3kFYaAqGG8grjL2nLU6EFMq9U0vDSAGKVFWpw2PZr9Q=");
             Twin twin = await rm.GetTwinAsync("dybronso-pnp-device", "PnPModule");
-            Console.WriteLine($"Got twin: {twin.ToJson(Formatting.Indented)}");
+            Twin twin2 = await rm.GetTwinAsync("dybronso-pnp-device", "PnPModule2");
+            Twin twin3 = await rm.GetTwinAsync("dybronso-pnp-device", "PnPModule3");
+            Twin twin4 = await rm.GetTwinAsync("dybronso-pnp-device", "PnPModule4");
+            Console.WriteLine($"Got twin1: {twin.ToJson(Formatting.Indented)}");
+            Console.WriteLine($"Got twin2: {twin2.ToJson(Formatting.Indented)}");
+            Console.WriteLine($"Got twin3: {twin3.ToJson(Formatting.Indented)}");
+            Console.WriteLine($"Got twin4: {twin4.ToJson(Formatting.Indented)}");
         }
     }
 }
